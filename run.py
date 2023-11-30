2b728a98729df7204bce9c70e8ceb6b3cb6a8938
XOZANAYKUT <>
2023-11-30 19:37:04 +0000

    Add print_question_and_word function

--- run.py
+++ run.py
@@ -68,3 +68,11 @@ hangman_stages = [
 def clear_screen():
     """Clears the terminal screen."""
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def print_question_and_word(chosen_item, guessed_letters):
+    """Displays the question and the word with correctly guessed letters."""
+    print(f"Question: {chosen_item['question']}" if 'question' in chosen_item else f"Hint: {chosen_item['hint']}")
+    
+    if 'answer' in chosen_item:
+        displayed_word = ''.join(letter if letter in guessed_letters else '_' for letter in chosen_item['answer'])
+        print(f"Word: {displayed_word}")    
